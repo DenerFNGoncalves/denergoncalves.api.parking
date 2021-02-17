14530a03dd8f5ae88a5e9d0559a43a33a40ea4d0
Dener Gonçalves <denergoncalvess@gmail.com>
2021-02-16 22:30:54 -0400

    feat(parking): added the functionality

--- src/app/controllers/parking.js
+++ src/app/controllers/parking.js
@@ -1,23 +1,30 @@
-
+import ParkingSpot from "../models/parking-spot"
 
 class ParkingController {
-
-  async insert({ data }) {
-    //TODO
-    return { success: true, spot: 5 }
+  async insert(data) {
+    return ParkingSpot.create(data.dataValues, {})
+      .then(spot => spot.id)
+      .catch(err => err)
   }
-
-
 }
 
 export default {
   async post(req, res) {
     const { body } = req
-    var result = await new ParkingController().insert({ body })
 
-    const status = result.err ? 500 : 201
+    return new ParkingController()
+      .insert(body)
+      .then(result => {
+        return res.status(201).json({
+          spot: result,
+          success: true
+        })
+      })
+      .catch(err => {
+        return res.status(500).json(err)
+      })
 
-    return res.status(status).json(result)
+
   }
 
 }
