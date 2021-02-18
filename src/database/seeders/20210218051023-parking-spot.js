76a7dbc16c5d92bb7c436cb749cdacac41f9f83b
Dener <denergoncalvess@gmail.com>
2021-02-18 01:25:11 -0400

    test(parking): added payment tests

--- src/database/seeders/20210218051023-parking-spot.js
+++ src/database/seeders/20210218051023-parking-spot.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
 module.exports = {
   up: async (iqry) => {
     return iqry.bulkInsert('parking_spot', [
       {
-        id: 1,
-        plate: 'ABC-1234',
+        id: 4,
+        plate: 'DDD-3333',
         paid: true,
         left: false,
         check_in: new Date(),
@@ -13,19 +13,10 @@ module.exports = {
         created_at: new Date(),
       },
       {
-        id: 2,
-        plate: 'CCC-7985',
+        id: 5,
+        plate: 'HAT-3451',
         paid: false,
         left: false,
-        check_in: new Date(),
-        check_out: new Date(),
-        created_at: new Date(),
-      },
-      {
-        id: 3,
-        plate: 'LOV-3111',
-        paid: true,
-        left: true,
         check_in: new Date(),
         check_out: new Date(),
         created_at: new Date(),
@@ -34,8 +25,9 @@ module.exports = {
   },
 
   down: async (iqry, Sequelize) => {
+    
     return iqry.bulkDelete('parking_spot', null, {
-      where: { "id": { [Sequelize.Op.between]: [1, 3] } }
-    })
+      where: { "id": { [Sequelize.Op.between]: [4, 5] } }
+    }) 
   }
-}
+};
